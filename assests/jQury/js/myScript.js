afe16ae4484de0f0e52e253f60abeddf55fed621
ChanukaWeerakkody <chanuka.weerakkody123@gmail.com>
2023-04-30 13:22:36 +0530

    content update

---
+++ assests/jQury/js/myScript.js
@@ -0,0 +1,21 @@
+// hide all content and load slides right to left
+// $("#customer").css('display','block');
+// $("#item").css('display','none');
+// $("#order").css('display','none');
+//
+// $("#linkCustomer").click(function (){
+//     $("#customer").css('display','block');
+//     $("#item").css('display','none');
+//     $("#order").css('display','none');
+// });
+//
+// $("#linkItem").click(function (){
+//     $("#customer").css('display','none');
+//     $("#item").css('display','block');
+//     $("#order").css('display','none');
+// });
+// $("#linkOrder").click(function (){
+//     $("#customer").css('display','none');
+//     $("#item").css('display','none');
+//     $("#order").css('display','block');
+// });
